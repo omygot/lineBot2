8e9967820da12acc7427318db767ffdd4fd84d65
omygot <55890142+omygot@users.noreply.github.com>
2019-09-28 12:25:30 +0700

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,6 @@ def callback():
     decoded = json.loads(json_line)
     user = decoded["events"][0]['replyToken']
     userText = decoded["events"][0]['message']['text']
-    #sendText(user,userText)
     if (userText == 'สวัสดี') :
         sendText(user,'สวัสดีจ้า')
     elif (userText == 'ไอ้บ้า') :
